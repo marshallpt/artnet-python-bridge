f05e272c3eec804a69baface1667d1fccf8bc57a
Patrick Marshall <patrickmarshall@pm.me>
2026-06-19 17:41:43 -0400

    got a full universe

--- god-help-me.py
+++ god-help-me.py
@@ -14,12 +14,12 @@ async def main():
         # Add a channel to the universe which consists of 3 values
         # Default size of a value is 8Bit (0..255) so this would fill
         # the DMX values 1..3 of the universe
-        for i in range(1, 512, 3):
-            channel = universe.add_channel(start=i, width=3)
+        for i in range(1, 512, 4):
+            channel = universe.add_channel(start=i, width=4)
 
             # Fade channel to 255,0,0 in 5s
             # The fade will automatically run in the background
-            channel.add_fade([255,255,0], 1000)
+            channel.add_fade([255,0,0,0], 1)
 
             # this can be used to wait till the fade is complete
             await channel

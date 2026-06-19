6ed1f611e5147eb68ebdc1c45c7dde7dc2dd9939
Patrick Marshall <patrickmarshall@pm.me>
2026-06-19 18:36:32 -0400

    feat: rings different colors

--- god-help-me.py
+++ god-help-me.py
@@ -1,28 +1,31 @@
 import asyncio
 import threading
-from pyartnet import ArtNetNode
+from pyartnet import ArtNetNode, Channel
 
 IP = '2.0.0.6'
-UNIVERSE_LIST = [199, 200, 201, 205, 206, 207, 211, 212, 217]
+INNER_UNIVERSE = [199, 200, 201, 205, 206, 207, 211, 212, 217]
+INNER_UNIVERSE = [211, 212, 217]
+OUTER_UNIVERSE = [199, 200, 201, 205, 206, 207]
 # GRBW
 ORANGE = [128, 255, 3, 0]
 BLUE = [112, 5, 252, 0]
 RED = [0, 255, 100, 0]
 
-async def set_color(channel, color):
+async def set_color(channel: Channel, color):
     channel.add_fade(color*128, 1000)
     await channel
 
+async def set_ring(node: ArtNetNode, universe_list, color):
+    for universe_id in universe_list:    
+        print(f"UNIVERSE: {universe_id}")
+        universe = node.add_universe(universe_id)
+        channel = universe.add_channel(start=1, width=512)
+
+        await set_color(channel=channel, color=color)
+
 async def main():
-
     async with ArtNetNode.create(IP, 6454) as node:
-
-        for universe_id in UNIVERSE_LIST:
-            
-            print(f"UNIVERSE: {universe_id}")
-            universe = node.add_universe(universe_id)
-            channel = universe.add_channel(start=1, width=512)
-
-            await set_color(channel=channel, color=BLUE)
+        await set_ring(node, INNER_UNIVERSE, BLUE)
+        await set_ring(node, OUTER_UNIVERSE, ORANGE)
 
 asyncio.run(main())

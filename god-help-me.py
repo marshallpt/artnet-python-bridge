28f82777857651a63cfdd56525b25c01b38ade46
Patrick Marshall <patrickmarshall@pm.me>
2026-06-19 18:43:16 -0400

    i fucking love everything

--- god-help-me.py
+++ god-help-me.py
@@ -8,6 +8,7 @@ INNER_UNIVERSE = [211, 212, 217]
 OUTER_UNIVERSE = [199, 200, 201, 205, 206, 207]
 
 # GRBW
+WHITE = [0, 0, 0, 255]
 ORANGE = [128, 255, 3, 0]
 BLUE = [112, 5, 252, 0]
 RED = [0, 255, 100, 0]
@@ -17,12 +18,18 @@ async def set_color(channel: Channel, color):
     await channel
 
 async def set_ring(node: ArtNetNode, universe_list, color):
+    tasks = []
+    
     for universe_id in universe_list:    
         print(f"UNIVERSE: {universe_id}")
         universe = node.add_universe(universe_id)
         channel = universe.add_channel(start=1, width=512)
 
-        await set_color(channel=channel, color=color)
+        tasks.append(
+            asyncio.create_task(set_color(channel=channel, color=color))
+        )
+
+    await asyncio.gather(*tasks)
 
 async def main():
     async with ArtNetNode.create(IP, 6454) as node:

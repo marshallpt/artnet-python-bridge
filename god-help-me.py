95dbb5869d50a5db9b43e5ca2701bf33d55d77eb
Patrick Marshall <patrickmarshall@pm.me>
2026-06-19 17:54:53 -0400

    got it working

--- god-help-me.py
+++ god-help-me.py
@@ -3,7 +3,8 @@ from pyartnet import ArtNetNode
 
 IP = '2.0.0.6'
 UNIVERSE_LIST = [199, 200, 201, 205, 206, 207, 211, 212, 217]
-COLOR = [255, 0, 0, 0]
+# GRBW
+COLOR = [0, 255, 255, 0]
 
 async def main():
 
@@ -15,7 +16,7 @@ async def main():
             universe = node.add_universe(universe_id)
             channel = universe.add_channel(start=1, width=512)
 
-            channel.add_fade(COLOR*128, 1)
+            channel.add_fade(COLOR*128, 500)
 
             await channel
 

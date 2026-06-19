b29616dcb3e96b736667c1096d5a0bb8208335d0
Patrick Marshall <patrickmarshall@pm.me>
2026-06-19 17:40:04 -0400

    got it

--- god-help-me.py
+++ god-help-me.py
@@ -14,13 +14,14 @@ async def main():
         # Add a channel to the universe which consists of 3 values
         # Default size of a value is 8Bit (0..255) so this would fill
         # the DMX values 1..3 of the universe
-        channel = universe.add_channel(start=1, width=3)
+        for i in range(1, 512, 3):
+            channel = universe.add_channel(start=i, width=3)
 
-        # Fade channel to 255,0,0 in 5s
-        # The fade will automatically run in the background
-        channel.add_fade([255,0,0], 1000)
+            # Fade channel to 255,0,0 in 5s
+            # The fade will automatically run in the background
+            channel.add_fade([255,255,0], 1000)
 
-        # this can be used to wait till the fade is complete
-        await channel
+            # this can be used to wait till the fade is complete
+            await channel
 
 asyncio.run(main())

029ee77f636f8d58c9333a8cb738eca991f37bfc
Patrick Marshall <patrickmarshall@pm.me>
2026-06-19 18:28:47 -0400

    add blue

--- god-help-me.py
+++ god-help-me.py
@@ -6,6 +6,7 @@ IP = '2.0.0.6'
 UNIVERSE_LIST = [199, 200, 201, 205, 206, 207, 211, 212, 217]
 # GRBW
 ORANGE = [128, 255, 3, 0]
+BLUE = [112, 5, 252, 0]
 RED = [0, 255, 100, 0]
 
 async def set_color(channel, color):
@@ -22,6 +23,6 @@ async def main():
             universe = node.add_universe(universe_id)
             channel = universe.add_channel(start=1, width=512)
 
-            await set_color(channel=channel, color=RED)
+            await set_color(channel=channel, color=BLUE)
 
 asyncio.run(main())
